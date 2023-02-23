93e9b6341669c631e019b8a506e24b01f3b92b21
Joan D <joandidachr@gmail.comm>
2023-02-24 00:29:06 +0100

    Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
 
     return (
  <nav className='navbar'>
-   <a href='#'><img src={logo} className="App-menu" alt="logo" /></a>
+   <a href='https://www.ironhack.com/en/barcelona?utm_campaign=BCN_Spain_Barcelona_Global_Search_Brand_EN&utm_source=google&utm_medium=cpc&utm_content=search-brand&utm_term=ironhack&gclid=Cj0KCQiAutyfBhCMARIsAMgcRJTIlrXGGw55pODp3Kl0kad-ZDDeNhBaRuzNXpvOU59paMfGMd6K0xgaAkeBEALw_wcB'><img src={logo} className="App-menu" alt="logo" /></a>
    <a href='#'><img src={menu} className="App-menu" alt="logo" /></a>
 
     </nav>
