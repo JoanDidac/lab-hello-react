d265145077519c30bc1ca45429407f004b93f992
Joan D <joandidachr@gmail.comm>
2023-02-24 00:28:14 +0100

    Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,23 @@
-import Navbar from "./Navbar"
-
+import Navbar from "./Navbar";
+import atom from "../images/logo512.png";
 
 const Header = () => {
 
-    return (
-        
-        <div className="header" >
-        <header>
-        <h1>Say hello to ReactJS</h1>
-        <p>You will learn how to use <br></br>the most popular frontend library, <br></br>and become a super Ninja developer</p>
-        </header>
+  return (
+    <div className="header">
+      <header>
+        <div className="header-text">
+          <h1>Say hello to ReactJS</h1>
+          <div id="atom">
+            <img id="atomo" className="App-logo" src={atom} alt="logo-React" />
+          </div>
+          <p>
+            You will learn how to use <br></br>the most popular frontend
+            library, <br></br>and become a super Ninja developer
+          </p>
         </div>
-    )
-}
+      </header>
+    </div>
+  );
+};
 export default Header;
--- src/components/Snippets.jsx
+++ src/components/Snippets.jsx
@@ -8,7 +8,7 @@ const Snippets = () => {
     return (
         <div className='snippet-container'>
           <div className='declarative'> <img src= {image1} alt='declarative' /> <h3>Declarative</h3> <p>React makes it painless to create interactive UI's.</p></div>
-          <div className='components'> <img src= {image2}  alt='components'/><h3>Compoonents</h3><p>Build encapsulated components that manage their state.</p></div>
+          <div className='components'> <img src= {image2}  alt='components'/><h3>Components</h3><p>Build encapsulated components that manage their state.</p></div>
           <div className='single-way'> <img src= {image3} alt='single' /> <h3>Single-Way</h3><p>A set of inmutable values are pass to each component.</p></div>
           <div className='jsx'> <img src= {image4}  alt='jsx'/><h3>JSX</h3><p>Statically-typed design to run on modern browsers.</p></div>
 
